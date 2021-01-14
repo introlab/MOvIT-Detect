9dd13986ba54eaec8ec0e41ce55565c6fe7996e7
Dominic Létourneau <dominic.letourneau@usherbrooke.ca>
2021-01-14 15:53:32 -0500

    Output rawData for frontend

--- python/fsm/ChairState.py
+++ python/fsm/ChairState.py
@@ -59,11 +59,13 @@ class TravelInformation:
         self.sensorName = 'default'
         self.isMoving = False
         self.lastDistance = float(0.0)
+        self.rawTravelData = {}
 
     def reset(self):
         self.sensorName = 'default'
         self.isMoving = False
         self.lastDistance = float(0.0)
+        self.rawTravelData = {}
 
     def to_dict(self):
         return {
@@ -92,6 +94,7 @@ class PressureInformation:
         self.centerOfGravityQ2 = {'x': float(0.0), 'y': float(0.0)}
         self.centerOfGravityQ3 = {'x': float(0.0), 'y': float(0.0)}
         self.centerOfGravityQ4 = {'x': float(0.0), 'y': float(0.0)}
+        self.rawPressureData = {}
 
     def reset(self):
         self.sensorName = 'default'
@@ -103,6 +106,7 @@ class PressureInformation:
         self.centerOfGravityQ2 = {'x': float(0.0), 'y': float(0.0)}
         self.centerOfGravityQ3 = {'x': float(0.0), 'y': float(0.0)}
         self.centerOfGravityQ4 = {'x': float(0.0), 'y': float(0.0)}
+        self.rawPressureData = {}
 
     def to_dict(self):
         return {
@@ -142,6 +146,8 @@ class AngleInformation:
         self.fIMUAngle = 0.0
         self.seatAngle = 0.0
         self.angleOffset = 0.0
+        self.fIMURawData = {}
+        self.mIMURawData = {}
 
     def reset(self):
         self.sensorName = 'default'
@@ -149,6 +155,8 @@ class AngleInformation:
         self.fIMUAngle = 0.0
         self.seatAngle = 0.0
         self.angleOffset = 0.0
+        self.fIMURawData = {}
+        self.mIMURawData = {}
 
     def to_dict(self):
         return {
@@ -179,24 +187,7 @@ class ChairState:
         self.Travel = TravelInformation()
         self.Pressure = PressureInformation()
         self.Angle = AngleInformation()
-
-        # const Coord_t POSITION_LOOKUP[9] = {{4.0f,4.0f}, {4.0f,0.0f}, {4.0f,-4.0f},
-        # {0.0f,4.0f}, {0.0f,0.0f}, {0.0f,-4.0f}, {-4.0f,4.0f}, {-4.0f,0.0f}, {-4.0f,-4.0f}};
-
-        # This should be modified with the type of pressure mat sensor
-        # units are in centimeters
-        # (0,0) is center of seat
-        self.positionLookup = [
-            {'x': 4.0, 'y': 4.0},
-            {'x': 4.0, 'y': 0.0},
-            {'x': 4.0, 'y': -4.0},
-            {'x': 0.0, 'y': 4.0},
-            {'x': 0.0, 'y': 0.0},
-            {'x': 0.0, 'y': -4.0},
-            {'x': -4.0, 'y': 4.0},
-            {'x': -4.0, 'y': 0.0},
-            {'x': -4.0, 'y': -4.0}
-        ]
+        self.AlarmRawData = {}
 
     def to_dict(self):
         return {
@@ -222,108 +213,91 @@ class ChairState:
     def to_json(self):
         return json.dumps(self.to_dict())
 
+    def raw_json_data(self):
+        """
+        Raw Data: (sensors/rawData)
+        {
+            "time": 1604434062,
+            "ToFSensor": {
+                "connected": 0,
+                "range": 260
+            },
+            "flowSensor": {
+                "connected": 0,
+                "travelX": 0,
+                "travelY": 0
+            },
+            "alarmSensor": {
+                "connected": 0,
+                "redLedOn": 0,
+                "redLedBlink": 0,
+                "greenLedOn": 0,
+                "greenLedBlink": 0,
+                "alternatingLedBlink": 0,
+                "motorOn": 0,
+                "buttonPressed": 0
+            },
+            "pressureMat": {
+                "threshold": 0,
+                "connected": 0,
+                "calibrated": 0,
+                "matData": [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+                ]
+            },
+            "mIMU": {
+                "connected": 0,
+                "calibrated": 0,
+                "accX": 0,
+                "accY": 0,
+                "accZ": 0,
+                "gyroX": 0,
+                "gyroY": 0,
+                "gyroZ": 0
+            },
+            "fIMU": {
+                "connected": 0,
+                "calibrated": 0,
+                "accX": 0,
+                "accY": 0,
+                "accZ": 0,
+                "gyroX": 0,
+                "gyroY": 0,
+                "gyroZ": 0
+            }
+        }
+        :param client:
+        :param messages:
+        :param chair_state:
+        :return:
+        """
+
+        # This is for ReactUI. 
+        rawData = {
+            "time": int(datetime.datetime.now().timestamp()),
+            "ToFSensor": self.Travel.rawTravelData,
+            "flowSensor": self.Travel.rawTravelData, 
+            "alarmSensor": self.AlarmRawData,
+            "pressureMat": self.Pressure.rawPressureData,
+            "mIMU": self.Angle.mIMURawData,
+            "fIMU": self.Angle.fIMURawData
+        }
+
+        return json.dumps(rawData)    
+
     def from_json(self, data: str):
         values = json.loads(data)
         self.from_dict(values)
 
-    def calculate_fIMU_angle(self, acc_x, acc_y, acc_z):
-        # Old implementation to be replaced by something better
-        y = -acc_x
-        x = math.sqrt(acc_y * acc_y + acc_z * acc_z)
-        # Convert radians to degrees
-        return math.atan2(y, x) * (180.0 / math.pi)
-
-    def calculate_mIMU_angle(self, acc_x, acc_y, acc_z):
-        # Old implementation to be replaced by something better
-        y = -acc_x
-        x = math.sqrt(acc_y * acc_y + acc_z * acc_z)
-        # Convert radians to degrees
-        return math.atan2(y, x) * (180.0 / math.pi)
-
-    def calculate_9x9_center_of_pressure(self, data: list, coordinates: list, threshold=0.0):
-        result = {'x': 0.0, 'y': 0.0, 'value': 0.0, 'isSeated': False}
-
-        # Make sure we have the right vector length
-        if len(data) == len(coordinates):
-            s = 0
-            position = {'x': 0.0, 'y': 0.0}
-
-            for i in range(len(data)):
-                position['x'] += data[i] * coordinates[i]['x']
-                position['y'] += data[i] * coordinates[i]['y']
-                s += data[i]
-
-            if s == 0:
-                position['x'] = 0.0
-                position['y'] = 0.0
-            else:
-                position['x'] = position['x'] / s
-                position['y'] = position['y'] / s
-
-            result['x'] = position['x']
-            result['y'] = position['y']
-            # Sum of all sensors
-            result['value'] = s
-
-            # TODO isSeated
-            if result['value'] > threshold:
-                result['isSeated'] = True
-
-        return result
-
-    def updateRawData(self, values: dict):
-        """
-            The data comes from the old C implementation. Let's calculate everything all at once.
-        """
-
-        # Reset everything
-        self.Pressure.reset()
-        self.Angle.reset()
-        self.Travel.reset()
-
-        if 'time' in values:
-            self.timestamp = values['time']
-
-        # Mobile IMU
-        if 'mIMU' in values and values['mIMU']['connected']:
-            self.Angle.mIMUAngle = self.calculate_mIMU_angle(values['mIMU']['accX'],
-                                                             values['mIMU']['accY'],
-                                                             values['mIMU']['accZ'])
-
-        # Fixed IMU
-        if 'fIMU' in values and values['fIMU']['connected']:
-            self.Angle.fIMUAngle = self.calculate_fIMU_angle(values['fIMU']['accX'],
-                                                             values['fIMU']['accY'],
-                                                             values['fIMU']['accZ'])
-
-        # Angle calculation
-        self.Angle.seatAngle = - (self.Angle.fIMUAngle - self.Angle.mIMUAngle - self.Angle.angleOffset)
-
-        if 'pressureMat' in values and 'matData' in values['pressureMat']:
-            # Calculate center of pressure
-            cop = self.calculate_9x9_center_of_pressure(values['pressureMat']['matData'],
-                                                        self.positionLookup,
-                                                        threshold=values['pressureMat']['threshold'])
-            self.Pressure.isSeated = cop['isSeated']
-            self.Pressure.centerOfGravity['x'] = cop['x']
-            self.Pressure.centerOfGravity['y'] = cop['y']
-            self.Pressure.centerOfGravityValue = cop['value']
-            # TODO Center of gravity per quadrant should be removed.
-
-        # Travel calculation
-        if 'flowSensor' in values and values['flowSensor']['connected']:
-            self.Travel.lastDistance += math.sqrt(math.pow(values['flowSensor']['travelX'], 2) +
-                                                  math.pow(values['flowSensor']['travelY'], 2))
-
-            # Are we moving?
-            # TODO hard coded threshold here...
-            if self.Travel.lastDistance > 25.0:
-                self.Travel.isMoving = True
-
-        # Snooze button
-        if 'alarmSensor' in values and values['alarmSensor']['connected']:
-            self.snoozeButton = values['alarmSensor']['buttonPressed']
-
+   
     def update_pressure_data(self, values: dict):
         # Reset values
         self.Pressure.reset()
@@ -338,6 +312,8 @@ class ChairState:
 
         if 'timestamp' in values:
             self.timestamp = max(values['timestamp'], self.timestamp)
+
+        self.Pressure.rawPressureData = values
 
         # TODO LOOK FOR THRESHOLD
         self.Pressure.isSeated = (self.Pressure.centerOfGravityValue > 0.0)
@@ -362,9 +338,18 @@ class ChairState:
         if 'seat_angle_offset' in values and connected:
             self.Angle.angleOffset = values['seat_angle_offset']
 
+        if 'mobile_imu' in values and connected:
+            self.Angle.mIMURawData = values['mobile_imu']
+
+        if 'fixed_imu' in values and connected:
+            self.Angle.fIMURawData = values['fixed_imu']
+
+
     def update_travel_data(self, values: dict):
         # Reset values
         self.Travel.reset()
+
+        self.Travel.rawTravelData = values
 
         if 'sensorName' in values:
             self.Travel.sensorName = values['sensorName']
@@ -385,6 +370,9 @@ class ChairState:
     def update_alarm_data(self, values: dict):
         connected = False
         self.snoozeButton = False
+
+        # Keep a copy of the alarm data 
+        self.AlarmRawData = values
 
         if 'connected' in values:
             connected = values['connected']
@@ -433,16 +421,6 @@ async def connect_to_mqtt_server(config):
             task = asyncio.create_task(log_messages(messages, "[unfiltered] {}"))
             tasks.add(task)
 
-            # Subscribe to topic(s)
-            # 🤔 Note that we subscribe *after* starting the message
-            # loggers. Otherwise, we may miss retained messages.
-            # TODO replace rawdata by individual topics
-            # await client.subscribe("sensors/rawData")
-            # manager = client.filtered_messages('sensors/rawData')
-            # messages = await stack.enter_async_context(manager)
-            # task = asyncio.create_task(handle_sensors_rawData(client, messages, chair_state))
-            # tasks.add(task)
-
             # Subscribe to pressure sensors
             await client.subscribe("sensors/pressure")
             manager = client.filtered_messages('sensors/pressure')
@@ -484,6 +462,10 @@ async def publish_chair_state(client, chair_state):
     while True:
         # print('publish chair state', chair_state)
         await client.publish('sensors/chairState', chair_state.to_json(), qos=2)
+
+        # publish rawData for React UI all in one data structure 
+        await client.publish('sensors/rawData', chair_state.raw_json_data(), qos=2)
+
         await asyncio.sleep(1)
 
 
@@ -492,78 +474,6 @@ async def log_messages(messages, template):
         # 🤔 Note that we assume that the message paylod is an
         # UTF8-encoded string (hence the `bytes.decode` call).
         print(template.format(message.payload.decode()))
-
-
-async def handle_sensors_rawData(client, messages, chair_state: ChairState):
-    """
-    Raw Data: (sensors/rawData)
-    {
-      "time": 1604434062,
-      "ToFSensor": {
-        "connected": 0,
-        "range": 260
-      },
-      "flowSensor": {
-        "connected": 0,
-        "travelX": 0,
-        "travelY": 0
-      },
-      "alarmSensor": {
-        "connected": 0,
-        "redLedOn": 0,
-        "redLedBlink": 0,
-        "greenLedOn": 0,
-        "greenLedBlink": 0,
-        "alternatingLedBlink": 0,
-        "motorOn": 0,
-        "buttonPressed": 0
-      },
-      "pressureMat": {
-        "threshold": 0,
-        "connected": 0,
-        "calibrated": 0,
-        "matData": [
-          0,
-          0,
-          0,
-          0,
-          0,
-          0,
-          0,
-          0,
-          0
-        ]
-      },
-      "mIMU": {
-        "connected": 0,
-        "calibrated": 0,
-        "accX": 0,
-        "accY": 0,
-        "accZ": 0,
-        "gyroX": 0,
-        "gyroY": 0,
-        "gyroZ": 0
-      },
-      "fIMU": {
-        "connected": 0,
-        "calibrated": 0,
-        "accX": 0,
-        "accY": 0,
-        "accZ": 0,
-        "gyroX": 0,
-        "gyroY": 0,
-        "gyroZ": 0
-      }
-    }
-    :param client:
-    :param messages:
-    :param chair_state:
-    :return:
-    """
-    async for message in messages:
-        # print('rawData', message.payload.decode())
-        values = json.loads(message.payload.decode())
-        chair_state.updateRawData(values)
 
 
 async def handle_sensors_pressure(client, messages, chair_state: ChairState):
